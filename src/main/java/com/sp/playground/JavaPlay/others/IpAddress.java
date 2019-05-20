44966d6480045d6e3b68fd63e88aaf805bdcab3b
sudhirpatil <spatil.sudhir@gmail.com>
2019-05-20 22:26:13 +0800

    regular expressions

--- src/main/java/com/sp/playground/JavaPlay/others/IpAddress.java
+++ src/main/java/com/sp/playground/JavaPlay/others/IpAddress.java
@@ -1,19 +1,51 @@
 package com.sp.playground.JavaPlay.others;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class IpAddress {
-    public static boolean isIpaddress(String ip){
-        boolean isIp = false;
-        if(!ip.contains(":")){
-            isIp = ip.matches("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$");
-        }else {
-            isIp = ip.matches("^[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}$");
+    public static String isIpaddress(String ipString){
+        String isIp = "Neither";
+        String v6Pattern = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
+        // 0-9 | 10-99 | 100-199 | 200-249 | 250-255
+//            String subPattern = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+        String v4Pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+        if(!ipString.contains(":")){
+            if(ipString.matches(v4Pattern)){
+                isIp = "IPv4";
+            }
+        }else if(ipString.matches(v6Pattern)){ //"^[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}$")){
+            isIp = "IPv6";
         }
 
         return isIp;
     }
 
+    public static String isIpAddrMatcher(String ipString){
+        String IPADDRESS_PATTERN =
+                "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+
+        Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+        Matcher matcher = pattern.matcher(ipString);
+        if (matcher.find()) {
+            return "IPv4";
+        }
+
+        return "Neither";
+    }
+
     public static void main(String[] args) {
-        System.out.println(isIpaddress("125.1.23.344"));
+//        System.out.println(isIpaddress("125.55.55.99"));
         System.out.println(isIpaddress("2001:0db8:0000:0000:0000:ff00:0042:8329"));
+
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        sc.nextLine();
+        while (count-- > 0){
+            String line = sc.nextLine();
+            System.out.println(isIpaddress(line));
+        }
     }
 }
