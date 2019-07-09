f14edcf35f3c56d4359a42769a5fc465f4ecb602
sudhirpatil <spatil.sudhir@gmail.com>
2019-07-09 08:02:38 +0800

    string practise

--- src/main/java/com/sp/playground/JavaPlay/hackerrank/strings/GenerateParenthesisLeetCode.java
+++ src/main/java/com/sp/playground/JavaPlay/hackerrank/strings/GenerateParenthesisLeetCode.java
@@ -2,7 +2,7 @@ package com.sp.playground.JavaPlay.hackerrank.strings;
 
 import java.util.*;
 
-public class ParenthesisPermutationsLeetCode {
+public class GenerateParenthesisLeetCode {
 
     /**
      * Key here is to select possible element at each stage of recursion.
@@ -10,11 +10,41 @@ public class ParenthesisPermutationsLeetCode {
      * 2. if opening size < closing size then one char from either be chosen as both permutations is required, so call recursion with one char from left and one char from right
      * 3. if opening is null just use char from close
      * 4. other conditions like open > close should not happen, if it is then some error case
+     *
+     * Establish how many recursive calls at each stage, e.g for loop while generating all possible permutations and here only 2 recursive calls with one choosing opening & another choosing closing
      * @param open
      * @param close
      * @param output
      * @return
      */
+
+
+    // Only one type of parenthesis so just keep count instead of String
+    public static List<String> getParenthesis(int open, int close, String output){
+        if(open == 0 && close == 0){
+            System.out.println("output : "+output);
+            return Arrays.asList(output);
+        }
+
+        List<String> list = new ArrayList<>();
+        if(open == close){
+            list.addAll(getParenthesis(open -1 , close, output+'('));
+        }else if(open < close && open !=0){
+            list.addAll(getParenthesis(open - 1, close, output+'('));
+            list.addAll(getParenthesis(open, close - 1, output+')'));
+        }else if(open == 0 && close >0) {
+            list.addAll(getParenthesis(open, close -1, output+')'));
+        }else {
+            System.out.println("Error: "+open+" "+close+" "+output);
+        }
+        return list;
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        return getParenthesis(3,3, "");
+    }
+
+    // Other one is more efficient
     public static List<String> getParenthesis(String open, String close, String output){
         if(open.length() == 0 && close.length() == 0){
             System.out.println("output : "+output);
@@ -37,6 +67,7 @@ public class ParenthesisPermutationsLeetCode {
     }
 
     public static void main(String[] args) {
-        System.out.println(getParenthesis("(((", ")))", ""));
+//        System.out.println(getParenthesis("(((", ")))", ""));
+        System.out.println(generateParenthesis(3));
     }
 }
