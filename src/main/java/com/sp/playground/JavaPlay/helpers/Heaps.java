bd4229430cad448a48000c81c1ee80fd42e32f4e
sudhirpatil <spatil.sudhir@gmail.com>
2020-03-08 08:13:45 +0800

    heaps notes

--- src/main/java/com/sp/playground/JavaPlay/helpers/Heaps.java
+++ src/main/java/com/sp/playground/JavaPlay/helpers/Heaps.java
@@ -4,22 +4,24 @@ import java.util.PriorityQueue;
 
 public class Heaps {
     public static void heapHelper(){
-        // Object should be compareable i.e implement compare
-        PriorityQueue<String> minDefaultPqueue = new PriorityQueue<>();
+        // minHeap -> to get min k elements, keeps max at element at the top, compare 1st - 2nd
+        // maxHeap -> to get max k elements, keeps min at the top, compare 2nd - 1st
+        // default priority queue is min heap
+        PriorityQueue<String> minHeap = new PriorityQueue<>();
 
         // Bounded priority queue with comparator, min heap
-        PriorityQueue<String> minHeap = new PriorityQueue<>(2, (e1, e2)-> Integer.compare(e1.length(), e2.length()));
+        PriorityQueue<String> minHeapCompare = new PriorityQueue<>(2, (e1, e2)-> Integer.compare(e1.length(), e2.length()));
         // Objects with Comparable interface can only be used, or else have to implement comparator object as 2nd param
         //PriorityQueue(int initialCapacity, Comparator<? super E> comparator)
 
-        minHeap.add("12345"); // add element priority Queue
-        minHeap.offer("123"); // add element, similar to adding Queue
-        minHeap.add("1234"); // add element priority Queue
-        minHeap.peek(); // check head in heap i.e min element
-        minHeap.poll(); // remove min element from heap
+        minHeapCompare.add("12345"); // add element priority Queue
+        minHeapCompare.offer("123"); // add element, similar to adding Queue
+        minHeapCompare.add("1234"); // add element priority Queue
+        minHeapCompare.peek(); // check head in heap i.e min element
+        minHeapCompare.poll(); // remove min element from heap
 
-        while (!minHeap.isEmpty()){
-            System.out.println(minHeap.poll()); // remove min element from heap
+        while (!minHeapCompare.isEmpty()){
+            System.out.println(minHeapCompare.poll()); // remove min element from heap
         }
 
         /* Imp
